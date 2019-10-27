347c48d29a8d2b7d9c64bcbf8e4fad60d9bf2665
Nik <nikvaes@gmail.com>
2019-10-27 15:09:54 +0100

    new command

--- gcp-docs/test_gpu.py
+++ gcp-docs/test_gpu.py
@@ -1,5 +1,17 @@
 import tensorflow as tf
 
-with tf.Session() as sess:
-    devices = sess.list_devices()
-    print(devices)
+# tf 1.14
+try:
+    with tf.Session() as sess:
+        devices = sess.list_devices()
+        print(devices)
+except:
+    print("not running tf 1.14")
+
+# tf 2
+
+try:
+    res = tf.test.is_gpu_available()
+    print(res)
+except:
+    print("not running tf 2.0")
