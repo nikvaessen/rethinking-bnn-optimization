aad15f6f4af87b34a401b97b283c46bd8541c336
Nik <nikvaes@gmail.com>
2019-10-27 15:41:43 +0100

    normal dependencies

--- setup.py
+++ setup.py
@@ -18,16 +18,16 @@ setup(
     packages=find_packages(),
     python_requires=">=3.5",
     license="Apache 2.0",
-    # install_requires=[
-    #     "click==7.0",
-    #     "tensorflow-datasets==1.0.2",
-    #     "larq==0.2.0",
-    #     "zookeeper==0.1.1",
-    # ],
-    # extras_require={
-    #     "tensorflow": ["tensorflow==1.14.0rc0"],
-    #     "tensorflow_gpu": ["tensorflow-gpu==1.14.0rc0"],
-    # },
+    install_requires=[
+        "click==7.0",
+        "tensorflow-datasets==1.0.2",
+        "larq==0.2.0",
+        "zookeeper==0.1.1",
+    ],
+    extras_require={
+        "tensorflow": ["tensorflow==1.14.0rc0"],
+        "tensorflow_gpu": ["tensorflow-gpu==1.14.0rc0"],
+    },
     entry_points="""
         [console_scripts]
         bnno=bnn_optimization.train:cli
